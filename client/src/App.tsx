cfe6de43cd875eb04e3e0345427bd0f80d09f85d
A13X4ND7A <alex@alexfaller.uk>
2022-10-02 16:02:45 +0100

    setup base files

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+	const hello = ['hello', 'yes', 'my']
+	return (
+		<>
+			<h1>Hello World with Typescript</h1>
+			<p>this is a test</p>
+		</>
+	)
+}
+
+export default App
